c67291213e92baa4c47c7102b07f04f257707420
Javier Alavrez <javieralvarez82@live.com>
2018-02-20 22:44:37 -0800

    Complted lesson 6 100%  passing.

--- Lesson6-JS-III/homework/README.md
+++ Lesson6-JS-III/homework/README.md
@@ -4,7 +4,7 @@
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
-	* Arrays
+	* Arrays = an array is an object that contains items that can be numbers, strings or both. An array can store multiple items that can accessed and sorted in a number of ways. Arrays are great for storing large amounts of items that need to be accessed later.
 
 2. 3. From the top level of your `Precourse` folder, run `npm test JSIII` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
 
--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -2,31 +2,53 @@
 
 function returnFirst(arr) {
   // return the first item from the array
+    return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+    return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+    return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+
+  for (let i = 0; i < arr.length; i++) {
+
+    arr[i] = arr[i] + 1; 
+
+  }
+
+  return arr;
+
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+
+   arr.push(item);
+
+  return arr;
+
+ 
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+
+    arr.unshift(item);
+  
+      return arr;
 }
 
 function wordsToSentence(words) {
@@ -34,27 +56,82 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+
+  let sentance = '';
+
+  for(let i = 0; i < words.length - 1; i++) {
+
+      sentance = sentance + words[i] + ' ';
+}
+        sentance = sentance + words[words.length - 1];
+          
+            return sentance;
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+
+  for(let i = 0; i <= arr.length; i++) {
+
+    if(arr[i] === item) {
+
+      return true;
+
+    }
+  
+    else {
+
+      return false;
+    }
+  } 
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+
+  let sumNumbers = 0;
+  
+  for(let i = 0; i < numbers.length; i++) {
+
+   sumNumbers = sumNumbers + numbers[i]; 
+
+  }
+
+  return sumNumbers;
+
+
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+
+  let average = 0;
+
+    for(let i = 0; i < testScores.length; i++) {
+        average = average + testScores[i];    
+    }
+
+    average = average / testScores.length;
+
+    return average;
+
+
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+
+ numbers = numbers.sort(function(a, b) {
+    return a - b;
+  });
+      return numbers[numbers.length - 1];
 }
+  
+
 
 // Do not modify code below this line.
 // --------------------------------
